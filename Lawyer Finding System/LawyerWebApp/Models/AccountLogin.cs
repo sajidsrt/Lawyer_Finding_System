ff6ca51fc2d304746c5a4d4d98c0d75360588d7d
Sazidul Islam <bsse0633@iit.du.ac.bd>
2017-11-27 08:36:05 +0000

    Add files via upload

---
+++ Lawyer Finding System/LawyerWebApp/Models/AccountLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LawyerWebApp.Models
+{
+    public class AccountLogin
+    {
+
+        [Required(ErrorMessage = "LawyerID is required")]
+        [Display(Name = "LawyerID")]
+        public string LawyerID { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType("password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ Lawyer Finding System/LawyerWebApp/Models/NormalUserLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LawyerWebApp.Models
+{
+    public class NormalUserLogin
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType("password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
